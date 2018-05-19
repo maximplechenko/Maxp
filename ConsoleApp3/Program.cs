705d901cac6855ec1de94c33d98dbd3a285d08ea
maxim.plechenko <maxim.plechenko@gmail.com>
2018-05-19 19:17:49 +0300

    Homework tasks: 1.5, 1.6

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,14 +7,14 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            //task 1.5
+            // Homework task 1.5
             Console.WriteLine("Maxim Plechenko");
             Console.WriteLine("Maxim\nPlechenko\n");
 
             Console.Write("Maxim Plechenko");
             Console.Write("\nMaxim\nPlechenko\n");
 
-            // task 1.6        
+            // Homework task 1.6        
             Console.Write(" ");
             Console.Write(" ");
             Console.Write(" ");
